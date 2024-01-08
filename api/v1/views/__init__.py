04a9d8a19bab6de2b36d9f209b5bf2fdbbe83752
ekarkat <ekarkat@gmail.com>
2024-01-08 22:54:37 +0000

    as3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,8 @@ from flask import Flask, Blueprint
 
 app_views = Blueprint('app_views', __name__)
 from api.v1.views.index import *
+""" this is for state"""
 from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
+# from api.v1.views.cities import *
+# from api.v1.views.amenities import *
 # from api.v1.views.users import *
