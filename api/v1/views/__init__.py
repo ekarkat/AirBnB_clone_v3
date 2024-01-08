08c81d8c56c22713db78fb59c47accdd628045bc
ekarkat <ekarkat@gmail.com>
2024-01-08 22:55:25 +0000

    tas3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,9 @@ app_views = Blueprint('app_views', __name__)
 from api.v1.views.index import *
 """ this is for state"""
 from api.v1.views.states import *
-# from api.v1.views.cities import *
-# from api.v1.views.amenities import *
-# from api.v1.views.users import *
+""" this is for state"""
+from api.v1.views.cities import *
+""" this is for state"""
+from api.v1.views.amenities import *
+""" this is for state"""
+from api.v1.views.users import *
