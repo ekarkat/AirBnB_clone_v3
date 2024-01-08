e80daf1651c18f04931df4afb356a2c402b7c1cf
ekarkat <ekarkat@gmail.com>
2024-01-08 23:04:22 +0000

    tas3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,7 +11,7 @@ app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 
 """ this is for state"""
-from api.v1.views.states import *
+# from api.v1.views.states import *
 
 """ this is for cities"""
 
