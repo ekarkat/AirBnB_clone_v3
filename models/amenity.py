7615928c6ed50bf92390d7273119e9b600292aa1
ekarkat <ekarkat@gmail.com>
2024-01-08 20:37:30 +0000

    tests

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,21 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import relationship
-from os import getenv
-from sqlalchemy.orm import backref
+#!/usr/bin/python
+""" holds class Amenity"""
 import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Class Amenity"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-        "Place", secondary="place_amenity", viewonly=True
-    )
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
