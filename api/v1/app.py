e9a4c12f8bdc54a144c3734551a99b94edd6c9f1
ekarkat <ekarkat@gmail.com>
2024-01-08 22:58:54 +0000

    tas3

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,8 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
-
+""" import cities """
+from api.v1.views.cities import *
 
 @app.teardown_appcontext
 def teardown_db(exception):
