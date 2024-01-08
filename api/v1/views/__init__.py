ef2264784e56c137dae5059dd79a2a368e9f26b4
ekarkat <ekarkat@gmail.com>
2024-01-08 23:07:20 +0000

    tas3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 '''the blueprint for the API.'''
 from flask import Flask, Blueprint
 
-app_views = Blueprint('app_views', __name__)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 # from api.v1.views.states import *
 # from api.v1.views.cities import *
