037358b4c91fba67c292443fb16ebb5b617f0747
ekarkat <ekarkat@gmail.com>
2024-01-10 00:54:49 +0000

    amen post

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -19,15 +19,15 @@ def amenity_api():
             am_dict.append(value.to_dict())
         return jsonify(am_dict)
 
-    # if request.method == 'POST':
-    #     data = request.get_json(force=True, silent=True)
-    #     if not data:
-    #         abort(400, "Not a JSON")
-    #     if "name" not in data:
-    #         abort(400, "Missing name")
-    #     amen = Amenity(**data)
-    #     amen.save()
-    #     return jsonify(amen.to_dict()), 201
+    if request.method == 'POST':
+        data = request.get_json(force=True, silent=True)
+        if not data:
+            abort(400, "Not a JSON")
+        if "name" not in data:
+            abort(400, "Missing name")
+        amen = Amenity(**data)
+        amen.save()
+        return jsonify(amen.to_dict()), 201
 
 
 # @app_views.route("/amenities/<amenity_id>", strict_slashes=False,
