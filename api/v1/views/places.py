1e235bc8ad3f08508bb6f1afa9fc5d84dd332c41
ekarkat <ekarkat@gmail.com>
2024-01-10 00:24:51 +0000

    task 10 all

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -41,32 +41,32 @@ def place_api(city_id):
         return jsonify(place.to_dict()), 201
 
 
-# @app_views.route('/places/<place_id>', strict_slashes=False,
-#                  methods=method)
-# def place_by_id(user_id):
-#     """ just a discription"""
-#     place = storage.get('Place', place_id)
-#     if not place:
-#         abort(404)
+@app_views.route('/places/<place_id>', strict_slashes=False,
+                 methods=method)
+def place_by_id(place_id):
+    """ just a discription"""
+    place = storage.get('Place', place_id)
+    if not place:
+        abort(404)
 
-#     if request.method == 'GET':
-#         return jsonify(place.to_dict())
+    if request.method == 'GET':
+        return jsonify(place.to_dict())
 
-#     elif request.method == 'DELETE':
-#         # Delete a state
-#         storage.delete(place)
-#         storage.save()
-#         return jsonify({}), 200
+    elif request.method == 'DELETE':
+        # Delete a state
+        storage.delete(place)
+        storage.save()
+        return jsonify({}), 200
 
-#     elif request.method == 'PUT':
-#         json_data = request.get_json(force=True, silent=True)
-#         if not json_data:
-#             abort(400, "Not a JSON")
-#         for key, value in json_data.items():
-#             if key == 'id' or key == 'created_at' or key == 'city_id'\
-#                     or key == 'updated_at' or key == 'user_id':
-#                 continue
-#             else:
-#                 setattr(place, key, value)
-#         place.save()
-#         return jsonify(place.to_dict()), 200
+    elif request.method == 'PUT':
+        json_data = request.get_json(force=True, silent=True)
+        if not json_data:
+            abort(400, "Not a JSON")
+        for key, value in json_data.items():
+            if key == 'id' or key == 'created_at' or key == 'city_id'\
+                    or key == 'updated_at' or key == 'user_id':
+                continue
+            else:
+                setattr(place, key, value)
+        place.save()
+        return jsonify(place.to_dict()), 200
